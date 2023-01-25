35e490ff9b6d7c76f6952a459533c062c33061e1
Marion Rose <marionrose@Marions-MacBook-Pro.local>
2023-01-25 08:39:55 -0600

    fixed header margin

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,19 +8,21 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="absolute w-full p-5 sm:px-10 md:px-20 md:py-10 max-w-5xl m-auto text-mint flex justify-between items-center uppercase text-xs font-sans font-semibold">
-      <MobileNav />
-      <DesktopNav />
-      <div className="flex items-center">
-        <a href="https://www.facebook.com/profile.php?id=100072203140159">
-          <BilFacebookCircle size="3em" />
-        </a>
-        <Link
-          href="#contact"
-          className="block bg-mint text-darkGreen px-4 py-2.5 rounded-xl ml-5"
-        >
-          <span>Contact</span>
-        </Link>
+    <header className="absolute w-full p-5 sm:px-10 md:px-20 md:py-10 text-mint  uppercase text-xs font-sans font-semibold">
+      <div className="flex justify-between items-center max-w-5xl m-auto">
+        <MobileNav />
+        <DesktopNav />
+        <div className="flex items-center">
+          <a href="https://www.facebook.com/profile.php?id=100072203140159">
+            <BilFacebookCircle size="3em" />
+          </a>
+          <Link
+            href="#contact"
+            className="block bg-mint text-darkGreen px-4 py-2.5 rounded-xl ml-5"
+          >
+            <span>Contact</span>
+          </Link>
+        </div>
       </div>
     </header>
   );
