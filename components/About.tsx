25789db2b0cc33085721d29bf822f89e40b6658e
Marion Rose <marionrose@marions-mbp.lan>
2023-01-26 12:35:39 -0600

    escaped apostrophe

--- components/About.tsx
+++ components/About.tsx
@@ -7,10 +7,9 @@ export default function About({}: Props) {
     <section className="bg-mint paddingX pt-32 pb-24">
       <h2 className="h2">About our Organization</h2>
       <p className="p text-darkGreen max-w-xl m-auto">
-        Our mission with the Be Kind for Ollie project is to honor Ollie’s life
-        by serving children through education, health, and other human service
-        initiatives. With your help, Ollie's radiant spirit can endure through
-        acts of kindness.
+        {
+          "Our mission with the Be Kind for Ollie project is to honor Ollie’s life by serving children through education, health, and other human service initiatives. With your help, Ollie's radiant spirit can endure through acts of kindness."
+        }
       </p>
     </section>
   );
