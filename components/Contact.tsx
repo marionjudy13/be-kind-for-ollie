3e03020df25ecad72739588af84092ca36b02408
Marion Rose <marionrose@Marions-MacBook-Pro.local>
2023-02-02 09:42:41 -0600

    Added placeholder form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -8,7 +8,7 @@ function Contact({}: Props) {
   return (
     <section className="bg-darkGreen padding" id="contact">
       <h2 className="h2 text-mint">Contact Us</h2>
-      <p className="p text-white mb-12">
+      <p className="p text-white mb-12 m-auto lg:max-w-xl">
         We would love to hear from you. We will respond to your message as soon
         as possible.
       </p>
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,143 +1,89 @@
 import React from "react";
-import { useState, useEffect } from "react";
-import { useForm } from "react-hook-form";
-import useWeb3Forms from "@web3forms/react";
-import { ThreeDots } from "react-loader-spinner";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 function Form({}: Props) {
-  const {
-    register,
-    handleSubmit,
-    reset,
-    watch,
-    control,
-    setValue,
-    formState: { errors, isSubmitSuccessful, isSubmitting },
-  } = useForm({
-    mode: "onTouched",
-  });
-  const [isSuccess, setIsSuccess] = useState(false);
-  const [message, setMessage] = useState(false);
-
-  // Please update the Access Key in the .env
-  const apiKey =
-    process.env.PUBLIC_ACCESS_KEY || "124ceb2f-d979-485c-9695-98a16e3aded1";
-
-  const { submit: onSubmit } = useWeb3Forms({
-    access_key: apiKey,
-    settings: {
-      from_name: "Dirty Carrots",
-      subject: "New Contact Message from Dirty Carrots",
-    },
-    onSuccess: (msg, data) => {
-      setIsSuccess(true);
-      setMessage(msg);
-      reset();
-    },
-    onError: (msg, data) => {
-      setIsSuccess(false);
-      setMessage(msg);
-    },
-  });
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) =>
+    (window.location.href = `mailto:marionejudy@gmail.com?subject=${formData.subject}&body="Hi, my name is ${formData.name}, ${formData.message}`);
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input
-        type="checkbox"
-        id=""
-        className="hidden"
-        style={{ display: "none" }}
-        {...register("botcheck")}
-      ></input>
-
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          autoComplete="false"
-          {...register("name", {
-            required: "Full name is required",
-            maxLength: 80,
-          })}
-        />
-        {/* <input
-          type="text"
-          autoComplete="false"
-          className={`${errors.name ? "error" : "no-error"}`}
-          {...register("name", {
-            required: "Full name is required",
-            maxLength: 80,
-          })}
-        /> */}
-        {errors.name && (
-          <div>
-            {/* <small className={style.small}>{errors.name.message}</small> */}
-          </div>
-        )}
-      </div>
-
-      <div>
-        <label htmlFor="email_address">Email Address</label>
-        <input
-          id="email_address"
-          type="email"
-          name="email"
-          autoComplete="false"
-          className={`${errors.email ? "error" : "no-error"}`}
-          {...register("email", {
-            required: "Enter your email",
-            pattern: {
-              value: /^\S+@\S+$/i,
-              message: "Please enter a valid email",
-            },
-          })}
-        />
-        {errors.email && (
-          <div>
-            <small>{errors.email.message}</small>
-          </div>
-        )}
-      </div>
-
-      <div>
-        <label htmlFor="message">Message</label>
-        <textarea
-          name="message"
-          className={`${errors.message ? "error" : "no-error"}`}
-          {...register("message", {
-            required: "Enter your Message",
-          })}
-        />
-        {errors.message && (
-          <div>
-            {" "}
-            <small>{errors.message.message}</small>
-          </div>
-        )}
-      </div>
-
-      <button type="submit">
-        {isSubmitting ? (
-          <ThreeDots
-            height="20"
-            width="50"
-            radius="20"
-            color="#fff"
-            ariaLabel="three-dots-loading"
-            visible={true}
-            wrapperStyle={{ marginBottom: 0 }}
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="w-full p text-start text-base lg:max-w-3xl m-auto"
+    >
+      <div className="md:flex flex-wrap">
+        <div className="md:w-1/2 md:pr-4">
+          <label htmlFor="firstName" className="label">
+            First Name
+          </label>
+          <input
+            {...register("name")}
+            name="firstName"
+            className="contactInput"
+            type="text"
           />
-        ) : (
-          "Send Message"
-        )}
+        </div>
+        <div className="md:w-1/2 md:pl-4">
+          <label htmlFor="lastName" className="label">
+            Last Name
+          </label>
+          <input
+            {...register("name")}
+            name="lastName"
+            className="contactInput"
+            type="text"
+          />
+        </div>
+      </div>
+      <div className="md:flex flex-wrap">
+        <div className="md:w-1/2 md:pr-4">
+          <label htmlFor="email" className="label">
+            Email Address
+          </label>
+          <input
+            {...register("email")}
+            name="email"
+            className="contactInput"
+            type="email"
+          />
+        </div>
+        <div className="md:w-1/2 md:pl-4">
+          <label htmlFor="email" className="label">
+            Message Subject
+          </label>
+          <input
+            {...register("subject")}
+            name="subject"
+            className="contactInput"
+            type="text"
+          />
+        </div>
+      </div>
+      <div>
+        <label htmlFor="message" className="label">
+          Message
+        </label>
+        <textarea
+          {...register("message")}
+          name="subject"
+          className="contactInput min-h-[200px] scroll mb-6 md:mb-12"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="button text-xs px-6 md:px-10  transition-colors ease-in-out hover:bg-white duration-500"
+      >
+        Submit
       </button>
-      {isSubmitSuccessful && isSuccess && (
-        <div>{message || "Success. Message sent successfully"}</div>
-      )}
-      {isSubmitSuccessful && !isSuccess && (
-        <div>{message || "Something went wrong. Please try later."}</div>
-      )}
     </form>
   );
 }
