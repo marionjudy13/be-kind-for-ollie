51122a408295b48ee2a106ee0e0279dcc05a48b2
Marion Rose <marionrose@marions-mbp.lan>
2023-04-21 08:14:05 -0500

    fixed about section bottom padding

--- components/About.tsx
+++ components/About.tsx
@@ -3,7 +3,7 @@ import { AboutContent } from "@/typings";
 
 export default function About({ header, paragraph }: AboutContent) {
   return (
-    <section className="bg-mint paddingX pt-40 pb-28" id="about">
+    <section className="bg-mint paddingX pt-40 pb-36" id="about">
       <h2 className="h2">About our Organization</h2>
       <p className="p text-darkGreen max-w-xl m-auto">{paragraph}</p>
     </section>
